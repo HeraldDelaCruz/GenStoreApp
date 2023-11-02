b4f021b595bd593809b44cc3cf953ff3ea3231a5
HeraldDC <herald.delacruz@collaberadigital.com>
2023-11-03 02:54:15 +0800

    3rd commit test

--- src/test/java/test/GenStoreLogin.java
+++ src/test/java/test/GenStoreLogin.java
@@ -1,13 +1,8 @@
 package test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 import org.testng.annotations.Test;
 
-import genstoreUtility.GenstoreUtils;
-import pages.GenStorePages;
 
 public class GenStoreLogin extends GenStoreBaseClass {
 	
